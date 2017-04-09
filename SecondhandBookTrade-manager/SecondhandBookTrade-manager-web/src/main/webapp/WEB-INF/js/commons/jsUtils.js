7aa294a0779014447594a875ee3e6675013b9e3c
zhoudl <18809428954@163.com>
2017-04-09 17:50:05 +0800

    系统JS工具类

--- SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/webapp/WEB-INF/js/commons/jsUtils.js
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/webapp/WEB-INF/js/commons/jsUtils.js
@@ -1,11 +1,62 @@
-/** 返回一个去掉首尾空格的数字 */
-trimNumber=function (numStr){
+/**
+ * 返回一个去掉首尾空格的数字
+ * @param numStr
+ * @returns
+ */
+trimNumber = function (numStr){
 	numStr = trimString(numStr);
-	if(numStr==""){return 0;}
-	else if(isNaN(parseFloat(numStr))){return 0;}
-	else{return parseFloat(numStr);}
+	if(numStr == ""){
+		return 0;
+	}else if(isNaN(parseFloat(numStr))){
+		return 0;
+	}else{
+		return parseFloat(numStr);
+	}
 };
-/** 返回一个去掉首尾空格的字符串 */
-trimString=function (str){
-	return (str==undefined || str==null) ? "" : (""+str).trim();
+/**
+ * 返回一个去掉首尾空格的字符串
+ * @param str
+ * @returns
+ */
+trimString = function (str){
+	return (str == undefined || str == null) ? "" : (""+str).trim();
+};
+/**
+ * 分到元的转换
+ * @param num
+ * @returns {Number}
+ */
+fen2yuan = function (num){
+	num = num*0.01;//分到元
+	num += '';//转成字符串
+	var reg=num.indexOf('.') >-1 ? /(\d{1,3})(?=(?:\d{3})+\.)/g : /(\d{1,3})(?=(?:\d{3})+$)/g;//千分符的正则
+	num = num.replace(reg, '$1,');//千分位格式化
+	return num;
+}
+/**
+ * LigerUIGrid 所需JSON数据格式转换
+ * @param jsonObj 
+ * @returns {___anonymous784_785}
+ */
+ligerUIDataGrid = function(jsonObj){
+	var tempJsonObj = {};
+	tempJsonObj.Rows = jsonObj.rows;
+	tempJsonObj.total = jsonObj.total;
+	return tempJsonObj;
+}
+/**
+ * 将json的key值转为大写
+ * @param jsonObj
+ * @returns
+ */
+upperJSONKey = function (jsonObj){  
+    for (var key in jsonObj){  
+        jsonObj["\""+key.toUpperCase()+"\""] = jsonObj[key];  
+        delete(jsonObj[key]);  
+    }  
+    return jsonObj;  
+}  
+/*时间戳转换成日期格式  重写toLocaleString()方法*/
+Date.prototype.toLocaleString = function() {
+    return this.getFullYear() + "年" + (this.getMonth() + 1) + "月" + this.getDate() + "日 " + this.getHours() + "点" + this.getMinutes() + "分" + this.getSeconds() + "秒";
 };
