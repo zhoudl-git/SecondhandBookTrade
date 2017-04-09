f19265de1888553396349fb5f0b06b5ae0171862
zhoudl <18809428954@163.com>
2017-04-09 17:50:41 +0800

    系统弹出框通用样式

--- SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/webapp/WEB-INF/js/commons/pop.js
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/webapp/WEB-INF/js/commons/pop.js
@@ -1,15 +1,15 @@
-/*
- * 提示信息
+/**
+ * 系统提示信息通用框
+ * @param str 提示文本内容
+ * @param status 状态标记 0 黑色无确认 1 对号图标无确认 2 需确认 3 询问框
+ * @param method 询问框点击确认后执行的方法名称(其他提示信息无需该参数)
  */
 function pop(str,status,method){
 	if(status == 0){
-		// 黑色提示 一般用于失败信息提示 提示完毕自动消失
 		layer.msg(str);
 	}else if(status == 1){
-		// 对号图标 一般用于操作成功提示 提示完毕自动消失
 		layer.msg(str, {icon: 1});
 	}else if(status == 2){
-		// 需要确认的提示信息框 提示完毕需要点击确定活关闭按钮才消失
 		layer.alert(str, {
 			  skin: 'layui-layer-molv' 
 				  // 样式类名 
@@ -19,7 +19,6 @@ function pop(str,status,method){
 			  ,anim: 4 //动画类型
 			});
 	}else if(status == 3){
-		// 询问框 一般用于删除等操作
 		layer.confirm(str, {
 			  btn: ['确定','取消'] //按钮
 			}, function(){
@@ -41,21 +40,36 @@ function pop(str,status,method){
 		layer.msg(str, {icon: 1});
 	}
 }
-/*加载中...*/
+/**
+ * 加载中...
+ * @returns
+ */
 function loading(){
 	var index = layer.load(1, {
 		shade: [0.1,'#fff'] //0.1透明度的白色背景
 	});
 	return index;
 }
+/**
+ * 加载中...
+ * @returns
+ */
 function loadingTO(){
 	var index = layer.load(0, {shade: false}); //0代表加载的风格，支持0-2
 	return index;
 }
+/**
+ * 关闭 加载中...
+ * @param index
+ */
 function loadindClose(index){
 	layer.close(index); 
 }
-/*正则式匹配出当前正在执行的方法名称*/
+/**
+ * 正则式匹配出当前正在执行的方法名称
+ * @param str 当前正在执行方法的字符串形式
+ * @returns
+ */
 function getFName(str){
 	var re = /function\s*(\w*)/i;
 	var matches = re.exec(str);
