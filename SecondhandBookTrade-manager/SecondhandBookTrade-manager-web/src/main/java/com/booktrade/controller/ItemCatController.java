c659b3e78c73fac4c0c0b1a3e79f13c5d8ba38dd
zhoudl <18809428954@163.com>
2017-04-09 18:26:12 +0800

    书籍类目操作控制器

---
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/java/com/booktrade/controller/ItemCatController.java
@@ -0,0 +1,36 @@
+package com.booktrade.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.booktrade.pojo.TreeNode;
+import com.booktrade.service.ItemCatService;
+
+
+/**   
+ * @ClassName:  ItemCatController   
+ * @Description:书籍分类管理   
+ * @author: zhoudl  
+ * @date:   2017年4月9日 下午6:06:29   
+ *      
+ */  
+@Controller
+@RequestMapping("/item/cat")
+public class ItemCatController {
+
+	@Autowired
+	private ItemCatService itemCatService;
+	
+	@RequestMapping("/list")
+	@ResponseBody
+	public List<TreeNode> getItemCatList(@RequestParam(value="id", defaultValue="0")Long parentId) {
+		List<TreeNode> list = itemCatService.getItemCatList(parentId);
+		return list;
+	}
+	
+}
