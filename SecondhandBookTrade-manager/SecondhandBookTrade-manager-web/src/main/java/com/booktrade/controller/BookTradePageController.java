7f2809e39c485bfe2e7654f50bb9c8a51ff90b3e
zhoudl <18809428954@163.com>
2017-04-09 18:25:35 +0800

    系统页面条状控制类

---
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-web/src/main/java/com/booktrade/controller/BookTradePageController.java
@@ -0,0 +1,41 @@
+package com.booktrade.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**   
+ * @ClassName:  BookTradePageController   
+ * @Description: 系统页面跳转控制   
+ * @author: zhoudl  
+ * @date:   2017年4月8日 上午11:05:20   
+ *      
+ */  
+@Controller
+public class BookTradePageController {
+
+	/**   
+	 * @Title: showIndex   
+	 * @Description: 展示首页   
+	 * @param: @return      
+	 * @return: String      
+	 * @throws   
+	 */  
+	@RequestMapping("/")
+	public String showIndex() {
+		return "index";
+	}
+	
+	/**   
+	 * @Title: showPage   
+	 * @Description: 展示功能页面  
+	 * @param: @param page
+	 * @param: @return      
+	 * @return: String      
+	 * @throws   
+	 */  
+	@RequestMapping("/{page}")
+	public String showPage(@PathVariable String page) {
+		return page;
+	}
+}
