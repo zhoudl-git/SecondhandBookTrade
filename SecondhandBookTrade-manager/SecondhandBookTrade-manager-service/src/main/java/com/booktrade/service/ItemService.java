c59b490eeecc0bda38f1acfe99b801ec2c89e90f
zhoudl <18809428954@163.com>
2017-04-23 09:58:40 +0800

    商品列表页删除及批量删除功能

--- SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/ItemService.java
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/ItemService.java
@@ -7,4 +7,6 @@ import com.booktrade.pojo.TbItem;
 public interface ItemService {
 	TbItem getItemById(Long itemId);
 	LigerUIDataGridResult getAllItem(Integer page,Integer rows);
+	int deleteBookById(Long bookId);
+	int deleteBooksByIdsOK(Long [] bookIds);
 }
