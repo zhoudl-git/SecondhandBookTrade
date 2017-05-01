b50afb56e90032ccded4ad73d2b8e6a36a352ab3
zhoudl <18809428954@163.com>
2017-05-01 13:19:15 +0800

    书籍新增功能实现

--- SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/ItemService.java
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/ItemService.java
@@ -2,11 +2,14 @@ package com.booktrade.service;
 
 
 import com.booktrade.pojo.LigerUIDataGridResult;
+import com.booktrade.pojo.SystemReturnResult;
 import com.booktrade.pojo.TbItem;
+import com.booktrade.pojo.TbItemDesc;
 
 public interface ItemService {
 	TbItem getItemById(Long itemId);
 	LigerUIDataGridResult getAllItem(Integer page,Integer rows);
 	int deleteBookById(Long bookId);
 	int deleteBooksByIdsOK(Long [] bookIds);
+	SystemReturnResult addItem(TbItem item, TbItemDesc itemDesc, String itemParams);
 }
--- SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/impl/ItemServiceImpl.java
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-service/src/main/java/com/booktrade/service/impl/ItemServiceImpl.java
@@ -1,17 +1,24 @@
 package com.booktrade.service.impl;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.booktrade.dao.TbItemDescMapper;
 import com.booktrade.dao.TbItemMapper;
+import com.booktrade.dao.TbItemParamItemMapper;
 import com.booktrade.pojo.LigerUIDataGridResult;
 import com.booktrade.pojo.SystemReturnResult;
 import com.booktrade.pojo.TbItem;
+import com.booktrade.pojo.TbItemDesc;
 import com.booktrade.pojo.TbItemExample;
 import com.booktrade.pojo.TbItemExample.Criteria;
+import com.booktrade.pojo.TbItemParamItem;
 import com.booktrade.service.ItemService;
+import com.booktrade.utils.ExceptionUtil;
+import com.booktrade.utils.IDUtils;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
 @Service
@@ -19,6 +26,13 @@ public class ItemServiceImpl implements ItemService {
 
 	@Autowired
 	private TbItemMapper itemMapper;
+	
+	@Autowired
+	TbItemDescMapper itemDescMapper;
+	
+	@Autowired
+	private TbItemParamItemMapper itemParamItemMapper;
+	
 	@Override
 	public TbItem getItemById(Long itemId) {
 		//TbItem item = itemMapper.selectByPrimaryKey(itemId);
@@ -40,7 +54,7 @@ public class ItemServiceImpl implements ItemService {
 		TbItemExample example = new TbItemExample();
 		//添加条件
 		//Criteria criteria = example.createCriteria();
-		//criteria.andIdEqualTo(123l);
+		//criteria.and;
 		List<TbItem> list = itemMapper.selectByExample(example);
 		//取total
 		PageInfo<TbItem> pageInfo = new PageInfo<>(list);
@@ -61,6 +75,40 @@ public class ItemServiceImpl implements ItemService {
 		int flag = itemMapper.deletesByPrimaryKey(bookIds);
 		return flag;
 	}
-
-
+	@Override
+	public SystemReturnResult addItem(TbItem item, TbItemDesc itemDesc, String itemParams) {
+		try {
+			//生成书籍id
+			//可以使用redis的自增长key，在没有redis之前使用时间+随机数策略生成
+			Long itemId = IDUtils.genItemId();
+			//补全不完整的字段
+			item.setId(itemId);
+			item.setStatus((byte) 1);
+			Date date = new Date();
+			item.setCreated(date);
+			item.setUpdated(date);
+			//把数据插入到书籍表
+			itemMapper.insert(item);
+			//添加书籍描述
+			itemDesc.setItemId(itemId);
+			itemDesc.setCreated(date);
+			itemDesc.setUpdated(date);
+			//把数据插入到书籍描述表
+			itemDescMapper.insert(itemDesc);
+			
+			//把书籍的规格参数插入到tb_item_param_item中
+			TbItemParamItem itemParamItem = new TbItemParamItem();
+			itemParamItem.setItemId(itemId);
+			itemParamItem.setParamData(itemParams);
+			itemParamItem.setCreated(date);
+			itemParamItem.setUpdated(date);
+			itemParamItemMapper.insert(itemParamItem);
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return SystemReturnResult.build(500, ExceptionUtil.getStackTrace(e));
+		}
+		
+		return SystemReturnResult.ok();
+	}
 }
