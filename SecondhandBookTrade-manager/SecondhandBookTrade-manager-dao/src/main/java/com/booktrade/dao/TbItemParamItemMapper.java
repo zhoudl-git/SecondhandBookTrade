dabd73a3f626b9df2e91cd1b7db1a048de7d5c67
zhoudl <18809428954@163.com>
2017-05-01 13:17:43 +0800

    书籍新增功能实现

---
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-dao/src/main/java/com/booktrade/dao/TbItemParamItemMapper.java
@@ -0,0 +1,38 @@
+package com.booktrade.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import com.booktrade.pojo.TbItemParamItem;
+import com.booktrade.pojo.TbItemParamItemExample;
+
+public interface TbItemParamItemMapper {
+    int countByExample(TbItemParamItemExample example);
+
+    int deleteByExample(TbItemParamItemExample example);
+
+    int deleteByPrimaryKey(Long id);
+
+    int insert(TbItemParamItem record);
+
+    int insertSelective(TbItemParamItem record);
+
+    List<TbItemParamItem> selectByExampleWithBLOBs(TbItemParamItemExample example);
+
+    List<TbItemParamItem> selectByExample(TbItemParamItemExample example);
+
+    TbItemParamItem selectByPrimaryKey(Long id);
+
+    int updateByExampleSelective(@Param("record") TbItemParamItem record, @Param("example") TbItemParamItemExample example);
+
+    int updateByExampleWithBLOBs(@Param("record") TbItemParamItem record, @Param("example") TbItemParamItemExample example);
+
+    int updateByExample(@Param("record") TbItemParamItem record, @Param("example") TbItemParamItemExample example);
+
+    int updateByPrimaryKeySelective(TbItemParamItem record);
+
+    int updateByPrimaryKeyWithBLOBs(TbItemParamItem record);
+
+    int updateByPrimaryKey(TbItemParamItem record);
+}
