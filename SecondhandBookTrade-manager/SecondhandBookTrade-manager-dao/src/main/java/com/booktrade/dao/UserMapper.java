a548e317d1e3a351d6ef3f757e1856c5e630b14f
zhoudl <18809428954@163.com>
2017-05-16 09:12:23 +0800

    用户Dao层

---
+++ SecondhandBookTrade-manager/SecondhandBookTrade-manager-dao/src/main/java/com/booktrade/dao/UserMapper.java
@@ -0,0 +1,14 @@
+package com.booktrade.dao;
+
+import java.util.List;
+import java.util.Map;
+
+import com.booktrade.pojo.User;
+
+public interface UserMapper {
+	public List<User> getAllUser();
+	public User getUserById(Long userId);
+	public int deleteUser(Map<String, String> map);
+	public int updateUser(Map<String, Object> map);
+	public int addUser(Map<String, Object> map);
+}
